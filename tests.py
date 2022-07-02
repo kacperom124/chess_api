1302fd2a55871039fee2604145d965e9ccd07947
kacper <kacperom124@gmail.com>
2022-07-02 16:36:33 +0200

    chess classes tests.

--- tests.py
+++ tests.py
@@ -7,6 +7,8 @@ import pytest
 from flask import Flask
 
 # Local
+from board import BoardField
+from figures import King, Rook, Bishop, Knight, Pawn
 from routes import configure_routes
 from validation import validate_position, validate_figure
 
@@ -35,6 +37,7 @@ def test_validate_position_false(test_input):
 @pytest.mark.parametrize('test_input', possible_figures)
 def test_validate_figure_True(test_input):
     assert validate_figure(test_input)
+
 
 possible_moves_data = [
     ('queen', 'h1', 200),
@@ -71,3 +74,96 @@ def test_possibility_of_move(figure, pos, dest, res):
     response = client.get(f'/api/v1/{figure}/{pos}/{dest}')
 
     assert response.status_code == res
+
+
+@pytest.mark.parametrize('x, y', [(1, 2)])
+def test_boardfield_class_true(x , y):
+    field_1 = BoardField.create_valid_board(x, y)
+    field_2 = BoardField.create_valid_board(x, y)
+    assert field_1 == field_2
+    assert str(field_1) == 'a2'
+
+
+@pytest.mark.parametrize('x, y', [(1, 2)])
+def test_boardfield_class_false(x, y):
+    field_1 = BoardField.create_valid_board(x, y)
+    field_2 = BoardField.create_valid_board(x + 1, y)
+    assert field_1 != field_2
+    assert str(field_1) != 'h2'
+
+
+@pytest.mark.parametrize('pos', ['a1'])
+def test_king_class(pos):
+    figure = King(pos)
+    moves = [
+        BoardField(1, 2),
+        BoardField(2, 1),
+        BoardField(2, 2),
+    ]
+    assert (figure.position.x == 1 and
+            figure.position.y == 1)
+    assert moves == figure.list_available_moves()
+
+
+@pytest.mark.parametrize('pos', ['a1'])
+def test_rook_class(pos):
+    figure = Rook(pos)
+    moves = [
+        BoardField(2, 1),
+        BoardField(3, 1),
+        BoardField(4, 1),
+        BoardField(5, 1),
+        BoardField(6, 1),
+        BoardField(7, 1),
+        BoardField(8, 1),
+        BoardField(1, 2),
+        BoardField(1, 3),
+        BoardField(1, 4),
+        BoardField(1, 5),
+        BoardField(1, 6),
+        BoardField(1, 7),
+        BoardField(1, 8),
+    ]
+    assert (figure.position.x == 1 and
+            figure.position.y == 1)
+    assert moves == figure.list_available_moves()
+
+
+@pytest.mark.parametrize('pos', ['a1'])
+def test_bishop_class(pos):
+    figure = Bishop(pos)
+    moves = [
+        BoardField(2, 2),
+        BoardField(3, 3),
+        BoardField(4, 4),
+        BoardField(5, 5),
+        BoardField(6, 6),
+        BoardField(7, 7),
+        BoardField(8, 8),
+    ]
+    assert (figure.position.x == 1 and
+            figure.position.y == 1)
+    assert moves == figure.list_available_moves()
+
+
+@pytest.mark.parametrize('pos', ['a1'])
+def test_knight_class(pos):
+    figure = Knight(pos)
+    moves = [
+        BoardField(2, 3),
+        BoardField(3, 2),
+    ]
+    assert (figure.position.x == 1 and
+            figure.position.y == 1)
+    assert moves == figure.list_available_moves()
+
+
+@pytest.mark.parametrize('pos', ['a1'])
+def test_pawn_class(pos):
+    figure = Pawn(pos)
+    moves = [
+        BoardField(1, 2),
+    ]
+    assert (figure.position.x == 1 and
+            figure.position.y == 1)
+    assert moves == figure.list_available_moves()
